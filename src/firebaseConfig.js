a2d4b3e38faadf8dab4521bacd774ce7e3d271b2
Daejin <qkreo321@gmail.com>
2025-11-27 12:08:03 +0900

    시작

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC473j9OJCWcH3L3UHu_5ysGCLHXf6ZloA",
+  authDomain: "sample1-3a574.firebaseapp.com",
+  projectId: "sample1-3a574",
+  storageBucket: "sample1-3a574.firebasestorage.app",
+  messagingSenderId: "1029330577746",
+  appId: "1:1029330577746:web:dd528a4480b7f3436a328b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
